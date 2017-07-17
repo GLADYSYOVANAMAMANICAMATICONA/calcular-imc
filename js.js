66b924dcddfc01594d4f843026b7d2836d8fa575
shey-esther <estheralvaro44@gmail.com>
2017-07-17 10:44:41 -0700

    enviando IMC

--- js.js
+++ js.js
@@ -1,9 +1,9 @@
-function calcularimc (){
 
+//embiando esther ejercicio de IMC
+
+function calcularimc (){
 var peso = prompt ("Ingrese su peso") ;    
 var altura = prompt ("Ingrese su talla");  
-
-  
 var imc = parseFloat(peso) / (parseFloat(altura) * parseFloat(altura));    
 return imc;
 }
