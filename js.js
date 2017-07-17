42116617ddde57583f2f1d470dee97b3c5fb5699
GLADYSYOVANAMAMANICAMATICONA <roxsy.lj@gmail.com>
2017-07-17 12:35:11 -0500

    primer calculo

---
+++ js.js
@@ -0,0 +1,10 @@
+function calcularimc (){
+
+var peso = prompt ("Ingrese su peso") ;    
+var altura = prompt ("Ingrese su talla");  
+
+  
+var imc = parseFloat(peso) / (parseFloat(altura) * parseFloat(altura));    
+return imc;
+}
+console.log (calcularimc());
